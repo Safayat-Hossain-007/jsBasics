33426a65582a9fb5f37f8a0ffd3e02df1f369e6c
Safayat <safayathossain40@gmail.com>
2024-03-01 02:05:57 +0600

    First commit

---
+++ hello.js
@@ -0,0 +1,12 @@
+var price =10.7;
+console.log(typeof price);
+var seenAfter =21;
+console.log(typeof seenAfter);
+var name = 'safayat hossain';
+console.log(typeof name);
+console.log(name);
+
+var ishot = true;
+var isRich =false;
+console.log(isRich);
+console.log(ishot);
---
+++ string.js
@@ -0,0 +1,24 @@
+// var som = "    Hello My name is safatat Hossain";
+// console.log(som.toLowerCase());
+// console.log(som.indexOf('name'));
+// console.log(som.trim());
+//  var num1 = 12;
+//  var num2 = '12.6';
+//  num2=parseFloat(num2);
+//  console.log(num1+num2);
+
+//  var num1 = 0.1;
+//  var num2 = 0.2;
+//  var sum = num1+num2;
+//  console.log(sum.toFixed(5));
+
+// var num5 = -5;
+// console.log(Math.abs(num5));
+// var price = 5.709784;
+// console.log(Math.round(price));
+// console.log(Math.ceil(price));
+// console.log(Math.floor(price));
+// console.log(Math.random());
+
+var random = Math.random()*100;
+console.log(Math.round(random));
