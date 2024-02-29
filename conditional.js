df191300bd5406625c9ed9444bad5122fd2ae6b7
Safayat <safayathossain40@gmail.com>
2024-03-01 02:24:35 +0600

    push

---
+++ conditional.js
@@ -0,0 +1,16 @@
+// var biscuitPrice =12;
+// if(biscuitPrice<10){
+//     console.log("I'll eat the biscuit.");
+// }
+// else{
+//     console.log("Cha is better !");
+// }
+var jobPaiso = true;
+var savingAmount = 5000;
+if(jobPaiso == true && savingAmount > 20000){
+    console.log("congrats");
+
+}
+else{
+    console.log("keep grinding");
+}
