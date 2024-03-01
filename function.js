9b89b5cd220967d659e2b2ed16b16b4c9683ba0b
Safayat <safayathossain40@gmail.com>
2024-03-01 17:34:33 +0600

    Hmm

--- function.js
+++ function.js
@@ -1,6 +1,22 @@
-function sayLoveYou(){
-    console.log("Hello");
+// function sayLoveYou(){
+//     console.log("Hello");
+// }
+// sayLoveYou();
+// var date = 14 ;
+// var place = sayLoveYou();
+
+function doubleIt(num){
+ var result = num*2;
+ return result;
 }
-sayLoveYou();
-var date = 14 ;
-var place = sayLoveYou();
+var first = doubleIt(250);
+var second = doubleIt(500);
+var total = first +second;
+console.log(total);
+
+function add(num1, num2){
+    var result =num1+num2;
+    return result;
+}
+var sum = add(1,2);
+console.log(sum);
---
+++ object.js
@@ -0,0 +1,19 @@
+var student ={
+    id: 20005,
+    phone: '01772633182',
+    name: 'Safyat'
+}
+var student1= {
+    name: 'Safayat',
+    phone:"2afhaerf",
+    id:'242342'
+}
+
+console.log(student);
+console.log(student1);
+var phoneNo= student.phone;
+console.log(phoneNo);
+student.phone = '312275';
+console.log(student);
+student.Cgpa ="3.85";
+console.log(student);
